ac5cec0c927f2e5f2194b80edef678ccb2e49574
Rivaroni <michaelrivaroni@gmail.com>
2023-09-11 22:47:33 -0700

    Added the camera ability and saves screenshot in unity screenshots folder

--- Assets/Scripts/ScreenshotButton.cs
+++ Assets/Scripts/ScreenshotButton.cs
@@ -1,9 +1,12 @@
 using UnityEngine;
 using System.Collections;
 using System.IO;
+using UnityEngine.UI;
 
 public class ScreenshotButton : MonoBehaviour
 {
+    public Image cameraImage;
+
     private void Start()
     {
         // Create the Screenshots folder if it doesn't exist in the project
@@ -13,18 +16,24 @@ public class ScreenshotButton : MonoBehaviour
 
     private void Update()
     {
-        
+
     }
 
     private IEnumerator Screenshot()
     {
         yield return new WaitForEndOfFrame();
-        
-        // Create a new Texture2D to capture the screen
-        Texture2D texture = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
 
-        // Read the pixels from the screen into the texture
-        texture.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
+        // Calculate the capture area to capture the left half of the screen
+        float captureWidth = Screen.width / 2; // Adjust the width to capture only the left half
+        float captureHeight = Screen.height;
+        float captureX = 0; // Start capturing from the left edge of the screen
+        float captureY = 0;
+
+        // Create a new Texture2D to capture the left half of the screen
+        Texture2D texture = new Texture2D((int)captureWidth, (int)captureHeight, TextureFormat.RGB24, false);
+
+        // Read the pixels from the screen into the texture within the specified area
+        texture.ReadPixels(new Rect(captureX, captureY, captureWidth, captureHeight), 0, 0);
         texture.Apply();
 
         // Generate a unique filename based on the current date and time
@@ -40,10 +49,13 @@ public class ScreenshotButton : MonoBehaviour
 
         // Destroy the texture to free up memory
         Destroy(texture);
+
+        cameraImage.enabled = true;
     }
 
     public void TakeScreenshot()
     {
         StartCoroutine("Screenshot");
+        cameraImage.enabled = false;
     }
 }
