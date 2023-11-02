c9769443e210d2aaff01642d64330e460c11d384
Rivaroni <michaelrivaroni@gmail.com>
2023-11-01 20:49:04 -0700

    Gallery scene can go back to main
    same as title duh

--- Assets/Scripts/GalleryLoader.cs
+++ Assets/Scripts/GalleryLoader.cs
@@ -16,15 +16,4 @@ public class GalleryLoader : MonoBehaviour
         SceneManager.LoadScene("Main");
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
